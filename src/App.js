a9479569f04cb34b4dccefa392ce9044b31857f5
Gabriel Silva <gabriel_silva@discente.ufg.br>
2020-08-21 18:52:20 -0300

    adiçao de regex para validar input

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {View, Text, StatusBar, TextInput, TouchableOpacity} from 'react-native';
+
+import styles from './style';
+
+function App() {
+  const [binaryNumber, setBinaryNumber] = useState('');
+  const [decimalNumber, setDecimalNumber] = useState(undefined);
+  const [validInput, setValidInput] = useState(true);
+
+  function convertBinToDec() {
+    const isValid = /\b[0-1]{1,8}\b/.test(binaryNumber);
+
+    if (!isValid) {
+      setValidInput(false);
+    } else {
+      setDecimalNumber(parseInt(binaryNumber, 2));
+      setValidInput(true);
+    }
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Bin2Dec Calculator</Text>
+
+        <View style={styles.inputBlock}>
+          <Text style={styles.label}>Binary Number</Text>
+          <TextInput
+            style={validInput ? styles.input : styles.invalidInput}
+            keyboardType="number-pad"
+            placeholder="Binary number here"
+            onChangeText={(number) => setBinaryNumber(number.toString())}
+          />
+          <Text style={styles.errorText}>
+            {!validInput ? 'Verifique os valores digitados!' : ''}
+          </Text>
+        </View>
+
+        <View style={styles.inputBlock}>
+          <Text style={styles.label}>Decimal Number</Text>
+          <Text style={styles.input}>{decimalNumber || 'Result Here'}</Text>
+        </View>
+
+        <TouchableOpacity onPress={convertBinToDec} style={styles.button}>
+          <Text style={styles.textButton}>Convert!</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+}
+
+export default App;
---
+++ src/style.js
@@ -0,0 +1,57 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#000',
+    fontSize: 18,
+    fontFamily: 'Roboto',
+    marginVertical: 30,
+  },
+  inputBlock: {
+    width: '90%',
+    marginVertical: 30,
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+  input: {
+    borderRadius: 5,
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 10,
+    height: 55,
+    alignItems: 'center',
+  },
+  invalidInput: {
+    borderRadius: 5,
+    borderColor: 'red',
+    borderWidth: 1,
+    padding: 10,
+    height: 55,
+    alignItems: 'center',
+  },
+  errorText: {
+    fontSize: 8,
+    color: 'red',
+  },
+  button: {
+    width: '90%',
+    backgroundColor: '#58E218',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 55,
+    borderRadius: 5,
+  },
+  textButton: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
